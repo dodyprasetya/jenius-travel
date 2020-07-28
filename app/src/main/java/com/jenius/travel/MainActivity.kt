ec7a77bb2260b780d31711e2057089fc44aec904
Dody Prasetya <dody.prasetya@btpn.com>
2020-07-28 16:20:14 +0700

    create ui login

--- app/src/main/java/com/jenius/travel/MainActivity.kt
+++ app/src/main/java/com/jenius/travel/MainActivity.kt
@@ -2,10 +2,20 @@ package com.jenius.travel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonSignIn.setOnClickListener {
+            // do something when button sign in is clicked
+            val username = inputUsername.text.toString()
+
+            Toast.makeText(this, "Hello ${username}, welcome back!",
+                Toast.LENGTH_LONG).show()
+        }
     }
 }
